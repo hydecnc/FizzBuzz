952b963cc11b2d33bdbef8a97ed0469909615026
Hyde Yoo <hydeyoo0716@gmail.com>
2025-09-09 15:48:31 -0400

    Finish task 4.2

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,6 +5,25 @@ public class Multiples {
         System.out.println(multiples(1000, 3, 5));
     }
 
+    public static int multiples() {
+        int n = 1000;
+        int a = 3;
+        int b = 5;
+        int i = 1;
+        int count = 0;
+        while (i < n) {
+            boolean dBa = i % a == 0;
+            boolean dBb = i % b == 0;
+            
+            if (dBa || dBb) {
+                count++;
+            }
+
+            i++;
+        }
+        return count;
+    }
+
     public static int multiples(int n, int a, int b) {
 
         int i = 1;
@@ -12,7 +31,7 @@ public class Multiples {
         while (i < n) {
             boolean dBa = i % a == 0;
             boolean dBb = i % b == 0;
-            
+
             if (dBa || dBb) {
                 count++;
             }
